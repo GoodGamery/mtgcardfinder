6f1ea3c4b399dba618a246c49f9df26d7e6c72ea
Justin Woolley <justinwoolley@gmail.com>
2017-08-01 01:21:37 -0700

    fix lint errors

--- src/cardfinder.js
+++ src/cardfinder.js
@@ -1,14 +1,16 @@
+'use strict';
+
 const exec = require('child_process').exec;
 
 const regexEscape = function(str) {
 	const escapeRegex = /[|\\{}()[\]^$+*?.]/g;
 	const subtitution = '\\$&';	
 	return str.replace(escapeRegex, subtitution);
-}
+};
 
 const getCardNameRegex = function(cardname) {
 	return regexEscape(cardname).replace(/\s+/g, '\\s');
-}
+};
 
 const multiverseUrl = 'http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=';
 
@@ -16,16 +18,16 @@ const cardname = process.argv[2];
 
 if (!cardname) {
 	console.log('Card name argument not specified.');
-	exit(1);
+	process.exit(1);
 }
 
 const searchUrl = `http://gatherer.wizards.com/Pages/Search/Default.aspx?name=+%5Bm/^${encodeURIComponent(getCardNameRegex(cardname))}$/%5D`;
 
 const command = `curl -L ${searchUrl} `;
 
-const cardImgRegex = /<div class="cardImage">\s+<img src="..\/..\/Handlers\/Image.ashx\?multiverseid=(\d+)/
+const cardImgRegex = /<div class="cardImage">\s+<img src="..\/..\/Handlers\/Image.ashx\?multiverseid=(\d+)/;
 
-exec(command, (error, stdout, sterr) => {
+exec(command, (error, stdout) => {
 	if (error) {
 		console.error('Error finding card: ' + error);
 		return;
