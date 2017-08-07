cec09419ffc28f4927ae0c6a2ccdf58b4939165d
Mark Diehr <mdiehr@gmail.com>
2017-08-06 20:19:33 -0400

    More tests, separate .eslintrc file for test dir

--- src/card-api.js
+++ src/card-api.js
@@ -8,20 +8,17 @@ const MtgData = require('./mtg-data');
 const getCardImage = (req, res, next) => {
   const card = MtgData.getCardFromQuery(req.query);
   if (!card || !card.multiverseid) {
-    console.log(`No card/multiverseid found for card "${req.query.card}"`);
     fs.createReadStream(path.join(__dirname, `..`, `static`, `images`, `cardback.jpg`))
       .on(`error`, (err) => next(err))
-      .pipe(res);
+      .pipe(res).type('image/jpeg');
   } else {
     const url = card.imageUrl;
     if (url.startsWith(`/static/`)) {
-      console.log(`Static Image "${req.query.card}" -> ${url}`);
       // Patch url for static image resources
       fs.createReadStream(path.join(__dirname, `..`, url))
         .on(`error`, (err) => next(err))
         .pipe(res);
     } else {
-      console.log(`Image "${req.query.card}" -> ${url}`);
       request.get(url).pipe(res);
     }
   }
@@ -38,8 +35,7 @@ const getCardHtml = (req, res) => {
 const getCardJson = (req, res) => {
   const card = MtgData.getCardFromQuery(req.query);
   if (!card)
-    return res.status(500).send(`No card found for card ${req.query.card}`);
-  console.log(`Card "${req.query.card}" -> ${card}`);
+    return res.status(404).send(`No card found for card ${req.query.card}`);
   res.send(card);
 };
 
--- test/test-server.js
+++ test/test-server.js
@@ -8,31 +8,80 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+const validateMtgJson = (res) => {
+  res.should.have.status(200);
+  res.type.should.equal(`application/json`);
+  res.body.should.be.a('object');
+  res.body.should.have.property('name');
+  res.body.should.have.property('multiverseid');
+  res.body.should.have.property('imageUrl');
+}
+
 describe('cardfinder', () => {
-  it('should return an image resource from GET /card/json?card=Plains', (done) => {
+  it('should return JSON from GET (plains) /card/json', (done) => {
     chai.request(server)
       .get('/card/json?card=Plains')
       .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('imageUrl');
+        validateMtgJson(res);
+        res.body.name.should.equal(`Plains`);
+        res.body.multiverseid.should.equal(129683);
+        res.body.imageUrl.should.equal('http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=129683');
         done();
       });
   });
-  it('should return an image resource from GET /card/image?card=Plains', (done) => {
+
+  it('should return JSON from GET (smallpox) /card/json', (done) => {
+    chai.request(server)
+      .get('/card/json?card=Smallpox')
+      .end((err, res) => {
+        validateMtgJson(res);
+        res.body.name.should.equal(`Smallpox`);
+        res.body.multiverseid.should.equal(237010);
+        res.body.imageUrl.should.equal('http://gatherer.wizards.com/Handlers/Image.ashx?type=card&multiverseid=237010');
+        done();
+      });
+  });
+
+  it('should return JSON from GET (smallpox, goof mode) /card/json', (done) => {
+    chai.request(server)
+      .get('/card/json?goof&card=Smallpox')
+      .end((err, res) => {
+        validateMtgJson(res);
+        // Response headers
+        res.body.name.should.equal(`Smallpox`);
+        res.body.multiverseid.should.equal(237010);
+        res.body.imageUrl.should.equal('/static/goofs/smallpox.png');
+        done();
+      });
+  });
+
+  it('should return 404 from GET (invalid cardname) /card/json', (done) => {
+    chai.request(server)
+      .get('/card/json?card=INVALID_NAME')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+
+  it('should return an image resource from GET /card/image', (done) => {
     chai.request(server)
       .get('/card/image?card=Plains')
       .end((err, res) => {
         res.should.have.status(200);
+        res.type.should.equal(`image/jpeg`);
+        res.body.length.should.above(10000);
         done();
       });
   });
-  it('should return an image resource from GET /card/image?card=INVALID_NAME', (done) => {
+
+  it('should return an image resource from GET /card/image with an invalid card name', (done) => {
     chai.request(server)
       .get('/card/image?card=INVALID_NAME')
       .end((err, res) => {
         res.should.have.status(200);
+        res.type.should.equal(`image/jpeg`);
+        res.body.length.should.above(10000);
         done();
       });
   });
